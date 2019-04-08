2f8085e83e991ac47603db562be0bd4645301922
YuriLukashenko <yuri.lukashenko@computools.com>
2019-04-08 21:14:14 +0300

    Add Models

--- CoVisiting.Data/Models/ApplicationUser.cs
+++ CoVisiting.Data/Models/ApplicationUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
-namespace CoVisiting.Models
+namespace CoVisiting.Data.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
---
+++ CoVisiting.Data/Models/TransportType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoVisiting.Data.Models
+{
+    public enum TransportType
+    {
+        Поїзд,
+        Автобус,
+        Літак,
+        Автостоп
+    }
+}
