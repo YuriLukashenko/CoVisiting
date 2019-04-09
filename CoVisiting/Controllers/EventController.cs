30412b7dd48f08de3fa53ab26e7ddedbb539fdb4
YuriLukashenko <yuri.lukashenko@computools.com>
2019-04-09 20:38:36 +0300

    Add event models and controller

---
+++ CoVisiting/Controllers/EventController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoVisiting.Data.Interfaces;
+using CoVisiting.Models.Event;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoVisiting.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEvent _eventService;
+
+        public EventController(IEvent eventService)
+        {
+            _eventService = eventService;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var newEvent = _eventService.GetById(id);
+
+            var model = new EventIndexModel
+            {
+
+            };
+
+            return View();
+        }
+    }
+}
---
+++ CoVisiting/Models/Event/EventIndexModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoVisiting.Data.Models;
+using CoVisiting.Models.Reply;
+
+namespace CoVisiting.Models.Event
+{
+    public class EventIndexModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string EventCity { get; set; }
+        public DateTime StartDateTime { get; set; }
+
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorImageUrl { get; set; }
+        public string AuthorRating { get; set; }
+        public DateTime Created { get; set; }
+
+        public string Content { get; set; }
+
+        public Moving BeforeEvent { get; set; }
+        public Moving AfterEvent { get; set; }
+
+        public IEnumerable<EventReplyModel> Replies { get; set; }
+
+    }
+}
