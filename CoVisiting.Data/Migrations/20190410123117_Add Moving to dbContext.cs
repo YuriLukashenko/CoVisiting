98ed4ef034821fba7761fe22b88ae9d61d416e70
YuriLukashenko <yuri.lukashenko@computools.com>
2019-04-10 20:26:00 +0300

    add not addded))

---
+++ CoVisiting.Data/Migrations/20190410123117_Add Moving to dbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoVisiting.Data.Migrations
+{
+    public partial class AddMovingtodbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CoVisiting/Models/Event/NewEventModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoVisiting.Data.Models;
+
+namespace CoVisiting.Models.Event
+{
+    public class NewEventModel
+    {
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+        public string AuthorName { get; set; }
+        public string CategoryImageUrl { get; set; }
+        public string Title { get; set; }
+        public string EventCity { get; set; }
+        public string EventPlace { get; set; }
+        public DateTime StartDateTime { get; set; }
+
+        public string Content { get; set; }
+
+        public TransportType TransportTypeBefore { get; set; }
+        public string DepartureCityBefore { get; set; }
+        public string ArrivalCityBefore { get; set; }
+        public DateTime DepartureTimeBefore { get; set; }
+        public DateTime ArrivalTimeBefore { get; set; }
+
+        public TransportType TransportTypeAfter { get; set; }
+        public string DepartureCityAfter { get; set; }
+        public string ArrivalCityAfter { get; set; }
+        public DateTime DepartureTimeAfter { get; set; }
+        public DateTime ArrivalTimeAfter { get; set; }
+    }
+}
